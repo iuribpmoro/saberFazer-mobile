5d786ebe19565db98680dd3e405c8fd260158e82
Iuri Moro <iuribpmoro@gmail.com>
2022-12-05 15:24:19 -0300

    Fixed authentication

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,17 +1,11 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import * as auth from "../services/authentication";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(null);
-
-    function isSigned() {
-        return new Promise((resolve, reject) => {
-            resolve(AsyncStorage.getItem("@saberFazer-token"));
-        });
-    }
 
     async function signIn(email, password) {
         const response = await auth.signIn(email, password);
@@ -30,8 +24,21 @@ export const AuthProvider = ({ children }) => {
         await AsyncStorage.removeItem('@saberFazer-token');
     }
 
+    useEffect(() => {
+        const isSignedIn = async () => {
+            const token = await AsyncStorage.getItem("@saberFazer-token");
+
+            return token;
+        };
+
+        isSignedIn().then((response) => {
+            console.log(response);
+            setToken(response);
+        });
+    }, []);
+
     return (
-        <AuthContext.Provider value={{ signed: !!isSigned(), token, signIn, signOut }}>
+        <AuthContext.Provider value={{ signed: !!token, token, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
 
