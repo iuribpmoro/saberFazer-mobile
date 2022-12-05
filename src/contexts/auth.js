cc3056f0ace2e45dc4e50baebd54e88e161b58e4
Iuri Moro <iuribpmoro@gmail.com>
2022-12-05 15:01:29 -0300

    Updated auth to use asyncstorage

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { createContext, useState } from "react";
 import * as auth from "../services/authentication";
 
@@ -6,11 +7,18 @@ const AuthContext = createContext({});
 export const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(null);
 
+    function isSigned() {
+        return new Promise((resolve, reject) => {
+            resolve(AsyncStorage.getItem("@saberFazer-token"));
+        });
+    }
+
     async function signIn(email, password) {
         const response = await auth.signIn(email, password);
 
         if (!response.message) {
             setToken(response.data);
+            await AsyncStorage.setItem('@saberFazer-token', response.data);
         } else {
             return response;
         }
@@ -19,10 +27,11 @@ export const AuthProvider = ({ children }) => {
 
     async function signOut() {
         setToken(null);
+        await AsyncStorage.removeItem('@saberFazer-token');
     }
 
     return (
-        <AuthContext.Provider value={{ signed: !!token, token, signIn, signOut }}>
+        <AuthContext.Provider value={{ signed: !!isSigned(), token, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
 
